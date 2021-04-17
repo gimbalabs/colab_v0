b50efd989ac575d730707b9aa329ebc80970bdb8
Piotr Napierala <piotr.napierala94@gmail.com>
2021-04-17 20:43:13 +0200

    chore: add offline-mirror packages folder
    Added offline-mirror yarn support to allow
    offline installation of node_modules.

--- src/contexts/profileContext.tsx
+++ src/contexts/profileContext.tsx
@@ -2,15 +2,15 @@
  * @name dataContext
  * @desc Context container for the data
  */
- import React, { useState, createContext } from 'react'; 
- import { BioState } from '../interfaces/profileInterface';
+import React, { useState, createContext } from "react";
+import { BioState } from "../interfaces/profileInterface";
 
 export const initialState: BioState = {
-  alias: 'Batman',
-  aboutURL: '',
-  imageURL:  '',
-  timeClockLengthMin:  null,
-  timeBlockCostADA:  null,
+  alias: "Batman",
+  aboutURL: "",
+  imageURL: "",
+  timeClockLengthMin: null,
+  timeBlockCostADA: null,
   setAlias: () => {},
   setAboutURL: () => {},
   setImageURL: () => {},
@@ -18,28 +18,37 @@ export const initialState: BioState = {
   setTimeBlockCostADA: () => {}
 };
 
-export const globalContext = createContext<BioState>(initialState)
+export const globalContext = createContext<BioState>(initialState);
 
-export const ContextProvider: React.FC = (props: any) => {
-  
-  const [alias, setAlias] = useState<string>('');
-  const [aboutURL, setAboutURL] = useState<string>('');
-  const [imageURL, setImageURL] = useState<string>('');
-  const [timeClockLengthMin, setTimeClockLengthMin] = useState<number | null>(null);
+type Props = {
+  children: React.ReactNode;
+};
+
+export const ContextProvider: React.FC = ({ children }: Props) => {
+  const [alias, setAlias] = useState<string>("");
+  const [aboutURL, setAboutURL] = useState<string>("");
+  const [imageURL, setImageURL] = useState<string>("");
+  const [timeClockLengthMin, setTimeClockLengthMin] = useState<number | null>(
+    null
+  );
   const [timeBlockCostADA, setTimeBlockCostADA] = useState<number | null>(null);
 
-  return <globalContext.Provider 
-            value={{
-                alias, 
-                aboutURL, 
-                imageURL, 
-                timeClockLengthMin,
-                timeBlockCostADA, 
-                setAlias, 
-                setAboutURL, 
-                setImageURL, 
-                setTimeClockLengthMin, 
-                setTimeBlockCostADA
-            }}>{props.children}</globalContext.Provider>
-}
-
+  return (
+    <globalContext.Provider
+      value={{
+        alias,
+        aboutURL,
+        imageURL,
+        timeClockLengthMin,
+        timeBlockCostADA,
+        setAlias,
+        setAboutURL,
+        setImageURL,
+        setTimeClockLengthMin,
+        setTimeBlockCostADA
+      }}
+    >
+      {children}
+    </globalContext.Provider>
+  );
+};
