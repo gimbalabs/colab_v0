049b470b537f46c387200b864d2a916bb1c2a2bc
James Dunseith <jdunseith@gimbalabs.io>
2021-04-20 10:37:58 -0400

    Build initial IDEA UI

---
+++ src/components/Ideas/EmptyIdeas.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {View, Text} from "react-native";
+
+export default function EmptyIdeas() {
+    return (
+        <View>
+            <Text>
+                Add Ideas Here
+            </Text>
+        </View>
+    )
+}
