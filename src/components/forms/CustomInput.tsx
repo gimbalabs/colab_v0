f98f40190c057f0b120d49b018ad64e751b3d768
Piotr Napierala <piotr.napierala94@gmail.com>
2021-04-22 21:53:54 +0200

    feat(form-organizer): create reusable input component

---
+++ src/components/forms/CustomInput.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import { TextInput, Text, StyleSheet } from "react-native";
+
+export const CustomInput: React.ReactNode = (props) => {
+  const {
+    field: { onChange, name, onBlur, value },
+    form: { errors, touched, setFieldTouched },
+    ...inputProps
+  } = props;
+
+  const hasError = errors[name] && touched[name];
+
+  return (
+    <>
+      <Text style={styles.inputLabel}>{props.label}</Text>
+      <TextInput
+        name={props.name}
+        style={[styles.input, hasError && styles.errorInput]}
+        value={value}
+        onChangeText={(text) => onChange(name)(text)}
+        onBlur={() => {
+          setFieldTouched(name);
+          onBlur(name);
+        }}
+        {...inputProps}
+      />
+      {hasError && <Text style={styles.errorInput}>{errors[name]}</Text>}
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    width: "90%",
+    marginHorizontal: "auto",
+    borderWidth: 1,
+    padding: 5,
+  },
+  errorInput: {
+    fontSize: 14,
+    color: "red",
+  },
+  inputLabel: {
+    paddingVertical: 10,
+  },
+});
