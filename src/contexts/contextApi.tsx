d0941544d392ba3b41ca363608ce9367327bef2f
Piotr Napierala <piotr.napierala94@gmail.com>
2021-05-16 17:16:19 +0200

    feat(my-calendar): Implement context API file.

---
+++ src/contexts/contextApi.tsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+
+import { AppContext } from "./appContext";
+import { MyCalendarContext } from "./myCalendarContext";
+import {
+  Availabilities,
+  CalendarHeader,
+  Month,
+  PreviewingDayEvents,
+  ScheduledEvent,
+} from "interfaces/myCalendarInterface";
+
+export const appContext = () => {
+  const { state, dispatch } = React.useContext(AppContext);
+
+  return {
+    auth: state.authentication,
+    toggleAuth: () => {
+      dispatch({ type: "TOGGLE_AUTH" });
+    },
+  };
+};
+
+export const myCalendarContext = () => {
+  const { state, dispatch } = React.useContext(MyCalendarContext);
+
+  return {
+    calendar: state.calendar,
+    calendarHeader: state.calendarHeader,
+    availabilities: state.availabilities,
+    scheduledEvents: state.scheduledEvents,
+    registrationDate: state.registrationDate,
+    previewingDayEvents: state.previewingDayEvents,
+    addEvent: (event: ScheduledEvent) => {
+      dispatch({ type: "ADD_EVENT", payload: event });
+    },
+    addAvailability: (availabilities: Availabilities[]) => {
+      dispatch({ type: "ADD_AVAILABILITY", payload: availabilities });
+    },
+    changeMonthHeader: (calendarHeader: CalendarHeader) => {
+      dispatch({ type: "CHANGE_MONTH_HEADER", payload: { calendarHeader } });
+    },
+    loadMyCalendar: (calendar: Month[]) => {
+      dispatch({ type: "LOAD_CALENDAR", payload: calendar });
+    },
+    previewDayEvents: (previewingDayEvents: PreviewingDayEvents) => {
+      dispatch({
+        type: "PREVIEW_DAY_EVENTS",
+        payload: { previewingDayEvents },
+      });
+    },
+  };
+};
