73d0402503e086fa9cafa2cffe186be53a91e765
Piotr Napierala <piotr.napierala94@gmail.com>
2021-05-29 21:55:05 +0200

    feat(onboarding): Create PricingScreen

--- src/screens/onboarding/PricingScreen.tsx
+++ src/screens/onboarding/PricingScreen.tsx
@@ -1,23 +1,190 @@
 import * as React from "react";
-import { SafeAreaView, View, Text, StyleSheet, Pressable } from "react-native";
+import { View, Text, StyleSheet, Pressable, ScrollView } from "react-native";
 
-import { Colors, Sizing, Typography } from "styles/index";
+import PagerView from "react-native-pager-view";
+import { CheckIcon } from "icons/index";
+import { Colors, Sizing, Typography, Outlines, Buttons } from "styles/index";
 
-export interface PricingScreenProps {}
+export interface PricingScreenProps {
+  pagerRef: React.RefObject<PagerView>;
+}
 
-export const PricingScreen = () => {
+export interface Benefit {
+  text: string;
+}
+const ATTENDEE_BENEFITS: Benefit[] = [
+  { text: "Vestibulum venenatis quam" },
+  { text: "Sapiente libero doloribus modi" },
+  { text: "Itaque cupiditate adipisci quibusdam" },
+  { text: "Vel ipsa esse repudiandae excepturi" },
+];
+
+const ORGANIZER_BENEFITS: Benefit[] = [
+  { text: "Vestibulum venenatis quam" },
+  { text: "Sapiente libero doloribus modi" },
+  { text: "Itaque cupiditate adipisci quibusdam" },
+  { text: "Vel ipsa esse repudiandae excepturi" },
+];
+
+export const PricingScreen = ({ pagerRef }: PricingScreenProps) => {
+  const renderCheckBoxes = (val: Benefit, i: number) => {
+    return (
+      <View key={i} style={styles.checkBox}>
+        <CheckIcon width={20} height={20} stroke={Colors.neutral.s500} />
+        <Text style={styles.checkBoxText}>{val.text}</Text>
+      </View>
+    );
+  };
+
+  const onAttendeePress = () => {
+    pagerRef.current?.setPage(2);
+  };
+  const onOrganizerPress = () => {
+    pagerRef.current?.setPage(2);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.headerText}>Welcome from pricing screen</Text>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        style={styles.scrollContainer}>
+        <View style={[styles.section, { marginBottom: Sizing.x50 }]}>
+          <View style={styles.labelContainer}>
+            <View style={styles.label}>
+              <Text style={styles.labelText}>Attendee</Text>
+            </View>
+          </View>
+          <View style={styles.card}>
+            <View style={styles.cardHeader}>
+              <Text style={styles.cardHeaderPrice}>1 ₳</Text>
+              <Text style={styles.cardHeaderText}>one time fee</Text>
+            </View>
+            <View style={styles.subHeader}>
+              <Text style={styles.subHeaderText}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </Text>
+            </View>
+            <View style={styles.checkBoxes}>
+              {ATTENDEE_BENEFITS.map(renderCheckBoxes)}
+            </View>
+          </View>
+          <Pressable
+            onPress={onAttendeePress}
+            style={Buttons.applyOpacity(styles.cardButton)}>
+            <Text style={styles.cardButtonText}>Continue as attendee</Text>
+          </Pressable>
+        </View>
+        <View style={[styles.section, { marginBottom: Sizing.x25 }]}>
+          <View style={styles.labelContainer}>
+            <View style={styles.label}>
+              <Text style={styles.labelText}>Organizer</Text>
+            </View>
+          </View>
+          <View style={styles.card}>
+            <View style={styles.cardHeader}>
+              <Text style={styles.cardHeaderPrice}>2 ₳</Text>
+              <Text style={styles.cardHeaderText}>/mo</Text>
+            </View>
+            <View style={styles.subHeader}>
+              <Text style={styles.subHeaderText}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </Text>
+            </View>
+            <View style={styles.checkBoxes}>
+              {ORGANIZER_BENEFITS.map(renderCheckBoxes)}
+            </View>
+          </View>
+          <Pressable
+            onPress={onOrganizerPress}
+            style={Buttons.applyOpacity(styles.cardButton)}>
+            <Text style={styles.cardButtonText}>Sign up as organizer</Text>
+          </Pressable>
+        </View>
+      </ScrollView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    width: "100%",
+    height: "95%",
+  },
   container: {
+    width: "90%",
+    justifyContent: "space-between",
+  },
+  section: {
     flex: 1,
+    justifyContent: "center",
+  },
+  labelContainer: {},
+  label: {
+    width: "35%",
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: Colors.primary.s200,
+    borderRadius: Outlines.borderRadius.large,
+  },
+  labelText: {
+    ...Typography.subHeader.x30,
+    lineHeight: Sizing.x25,
+    fontFamily: "Roboto-Bold",
+    color: Colors.primary.s600,
+  },
+  card: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: Sizing.x14,
+    padding: Sizing.x25,
+    backgroundColor: Colors.primary.neutral,
+    borderRadius: Outlines.borderRadius.large,
+  },
+  cardHeader: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "baseline",
+  },
+  cardHeaderPrice: {
+    ...Typography.header.x70,
+    fontFamily: "Roboto-Black",
+    lineHeight: 0,
+    letterSpacing: -1,
+    color: Colors.primary.s800,
+  },
+  cardHeaderText: {
+    ...Typography.header.x40,
+    fontFamily: "Roboto-Medium",
+    lineHeight: 0,
+    color: Colors.neutral.s500,
+    letterSpacing: -1,
+    marginLeft: Sizing.x5,
+  },
+  subHeader: {
+    width: "100%",
+  },
+  subHeaderText: {
+    ...Typography.body.x30,
+  },
+  checkBoxes: {
+    width: "95%",
+    alignItems: "center",
+    marginTop: Sizing.x10,
+  },
+  checkBox: {
+    width: "100%",
+    flexDirection: "row",
+    marginTop: Sizing.x3,
+  },
+  checkBoxText: {
+    ...Typography.body.x20,
+    marginLeft: Sizing.x10,
+  },
+  cardButton: {
+    ...Buttons.bar.transparent,
+  },
+  cardButtonText: {
+    ...Buttons.barText.transparent,
   },
   headerText: {
     ...Typography.header.x40,
