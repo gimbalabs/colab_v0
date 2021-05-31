9f0a9b405d4571a6e050a02701fdfd37b0e0b3d3
Piotr Napierala <piotr.napierala94@gmail.com>
2021-05-31 21:49:23 +0200

    feat(onboarding): finish initial UI - UserDetailScreen & WalletSetUpScreen

--- src/screens/onboarding/UserDetailsScreen.tsx
+++ src/screens/onboarding/UserDetailsScreen.tsx
@@ -1,70 +1,164 @@
 import * as React from "react";
-import { View, StyleSheet, Text, Pressable, TextInput } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  Pressable,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from "react-native";
 
 import { CustomPlainInput } from "components/forms/CustomPlainInput";
-import { CalendarIcon, CheckIcon, PencilAltIcon } from "icons/index";
+import { CalendarIcon, DownIcon, PencilAltIcon } from "icons/index";
+import {
+  Typography,
+  Colors,
+  Sizing,
+  Outlines,
+  Buttons,
+  Forms,
+} from "styles/index";
 
 export interface UserDetailScreenProps {}
 
 export const UserDetailsScreen = ({}) => {
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>Tell us a little bit about you</Text>
-      </View>
-      <View style={styles.formContainer}>
-        <CustomPlainInput
-          label="Profession"
-          placeholder="Doctor, therapist, developer, etc..."
-          icon={CheckIcon}
-          textContentType="jobTitle"
-          onPressHandler={() => {}}
-        />
-      </View>
-      <View style={styles.formContainer}>
-        <CustomPlainInput
-          label="Job Title"
-          placeholder="Full Stack Engineer, Sr Business..."
-          icon={CheckIcon}
-          onPressHandler={() => {}}
-        />
-      </View>
-      <View style={styles.formContainer}>
-        <CustomPlainInput
-          label="About yourself"
-          multiline={true}
-          placeholder="Passionate in helping others draw business goals and needs..."
-          icon={PencilAltIcon}
-          onPressHandler={() => {}}
-        />
-      </View>
-      <View style={styles.formContainer}>
-        <CustomPlainInput
-          label="Availability"
-          placeholder="weekly, weekends, mornings, e..."
-          icon={CalendarIcon}
-          onPressHandler={() => {}}
-        />
-      </View>
-      <View style={styles.formContainer}>
-        <View style={styles.label}>
-          <Text style={styles.labelText}>Hourly rate</Text>
+    <KeyboardAvoidingView
+      keyboardVerticalOffset={40}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}>
+      <ScrollView keyboardShouldPersistTaps="handled" style={styles.scrollView}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Tell us a little bit about you</Text>
         </View>
-        <View style={styles.inputWrapper}>
-          <TextInput placeholder="$35 an hour" />
-          <Pressable onPress={() => {}} style={styles.iconButton}></Pressable>
+        <View style={styles.formContainer}>
+          <CustomPlainInput
+            label="Profession"
+            placeholder="Doctor, therapist, developer, etc..."
+            icon={DownIcon}
+            styles={styles}
+            onPressHandler={() => {}}
+          />
+          <CustomPlainInput
+            label="Job Title"
+            placeholder="Full Stack Engineer, Sr Business..."
+            icon={DownIcon}
+            styles={styles}
+            onPressHandler={() => {}}
+          />
+          <CustomPlainInput
+            label="About yourself"
+            multiline={true}
+            numberOfLines={2}
+            placeholder="Passionate in helping others draw business goals and needs..."
+            icon={PencilAltIcon}
+            styles={styles}
+            onPressHandler={() => {}}
+          />
+          <CustomPlainInput
+            label="Availability"
+            placeholder="weekly, weekends, mornings, e..."
+            icon={CalendarIcon}
+            styles={styles}
+            onPressHandler={() => {}}
+          />
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={styles.inputContainer}>
+            <View style={styles.labelContainer}>
+              <Text style={styles.label}>Hourly rate</Text>
+            </View>
+            <View style={styles.textInputWrapper}>
+              <TextInput
+                style={styles.input}
+                keyboardType="numeric"
+                textContentType="none"
+                placeholder="35 ₳ an hour"
+                placeholderTextColor={styles.placeholderText.color}
+              />
+            </View>
+          </KeyboardAvoidingView>
+          <CustomPlainInput
+            label="Skills"
+            placeholder="Organized, Motivated, Critical Th..."
+            icon={DownIcon}
+            styles={styles}
+            onPressHandler={() => {}}
+          />
         </View>
-      </View>
-      <View style={styles.formContainer}>
-        <CustomPlainInput
-          label="Skills"
-          placeholder="Organized, Motivated, Critical Th..."
-          icon={CheckIcon}
-          onPressHandler={() => {}}
-        />
-      </View>
-    </View>
+        <Pressable
+          onPress={() => {}}
+          accessibilityLabel="Next"
+          style={Buttons.applyOpacity(styles.submitButton)}>
+          <Text style={styles.submitButtonText}>Next</Text>
+        </Pressable>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    width: "90%",
+  },
+  scrollView: {
+    width: "100%",
+    height: "100%",
+  },
+  header: {
+    marginVertical: Sizing.x10,
+  },
+  headerText: {
+    ...Typography.header.x70,
+    color: Colors.primary.neutral,
+  },
+  formContainer: {
+    flex: 1,
+    marginVertical: Sizing.x10,
+  },
+  submitButton: {
+    ...Buttons.bar.transparent,
+  },
+  submitButtonText: {
+    ...Buttons.barText.transparent,
+  },
+  /**
+   * styles passed as prop to CustomPlainInput
+   */
+  inputContainer: {
+    flex: 1,
+    width: "100%",
+    alignItems: "center",
+    marginBottom: Sizing.x10,
+  },
+  labelContainer: {
+    width: "100%",
+    paddingHorizontal: Sizing.x12,
+  },
+  label: {
+    ...Forms.inputLabel.primary,
+  },
+  textInputWrapper: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  input: {
+    width: "100%",
+    ...Forms.input.primary,
+  },
+  placeholderText: {
+    color: Colors.primary.s300,
+  },
+  iconWrapper: {
+    left: -45,
+    width: Sizing.x40,
+    height: Sizing.x40,
+  },
+  icon: {
+    width: Sizing.x35,
+    height: Sizing.x35,
+  },
+});
--- src/screens/onboarding/WalletSetUpScreen.tsx
+++ src/screens/onboarding/WalletSetUpScreen.tsx
@@ -1,28 +1,161 @@
 import * as React from "react";
-import { SafeAreaView, View, Text, StyleSheet, Pressable } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  Pressable,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  Dimensions,
+} from "react-native";
 
-import { Colors, Sizing, Typography } from "styles/index";
+import { CustomPlainInput } from "components/forms/CustomPlainInput";
+import { QrCodeSampleIcon, DuplicateIcon } from "icons/index";
+import {
+  Typography,
+  Colors,
+  Sizing,
+  Outlines,
+  Buttons,
+  Forms,
+} from "styles/index";
 
 export interface WalletSetUpScreenProps {}
 
+const SCREEN_WIDTH = Dimensions.get("screen").width;
+
 export const WalletSetUpScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.headerText}>
-        Welcome from organizer-detail screen
-      </Text>
-    </View>
+    <KeyboardAvoidingView
+      keyboardVerticalOffset={40}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}>
+      <ScrollView keyboardShouldPersistTaps="handled" style={styles.scrollView}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Set up your wallet</Text>
+          <Text style={styles.subHeaderText}>
+            Deposit /withdrawl ADA into your wallet to make payments, and
+            receive payments.
+          </Text>
+        </View>
+        <View style={styles.main}>
+          <View style={styles.qrCodeContainer}>
+            <QrCodeSampleIcon style={styles.qrIcon} />
+          </View>
+        </View>
+        <View style={styles.formContainer}>
+          <CustomPlainInput
+            label="Address"
+            placeholder="addr9czf30t9dzbdsxe79a2vtf8io"
+            icon={DuplicateIcon}
+            styles={styles}
+            onPressHandler={() => {}}
+          />
+          <CustomPlainInput
+            label="Amount"
+            placeholder="$2.00"
+            styles={styles}
+            keyboardType="numeric"
+            onPressHandler={() => {}}
+          />
+        </View>
+        <Pressable
+          onPress={() => {}}
+          accessibilityLabel="Next"
+          style={Buttons.applyOpacity(styles.submitButton)}>
+          <Text style={styles.submitButtonText}>Deposit</Text>
+        </Pressable>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    width: "90%",
+    height: "100%",
+  },
+  header: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    width: "100%",
+    marginVertical: Sizing.x10,
+  },
+  scrollView: {
+    width: "100%",
+    height: "100%",
   },
   headerText: {
-    ...Typography.header.x40,
+    ...Typography.header.x70,
     color: Colors.primary.neutral,
+    marginBottom: Sizing.x5,
+  },
+  main: {
+    flex: 2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  qrCodeContainer: {
+    flex: 1,
+    width: SCREEN_WIDTH * 0.65,
+    height: SCREEN_WIDTH * 0.65,
+    padding: Sizing.x15,
+    marginVertical: Sizing.x10,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: Colors.primary.neutral,
+    borderRadius: Outlines.borderRadius.base,
+  },
+  qrIcon: {},
+  subHeaderText: {
+    ...Typography.subHeader.x40,
+    fontFamily: "Roboto-Regular",
+    color: Colors.primary.neutral,
+  },
+  formContainer: {
+    flex: 1,
+    marginVertical: Sizing.x10,
+    justifyContent: "flex-start",
+  },
+  submitButton: {
+    ...Buttons.bar.transparent,
+  },
+  submitButtonText: {
+    ...Buttons.barText.transparent,
+  },
+  /**
+   * styles passed as prop to CustomPlainInput
+   */
+  inputContainer: {
+    width: "100%",
+    marginBottom: Sizing.x10,
+  },
+  labelContainer: {
+    width: "100%",
+    paddingHorizontal: Sizing.x12,
+  },
+  label: {
+    ...Forms.inputLabel.primary,
+  },
+  textInputWrapper: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  input: {
+    width: "100%",
+    ...Forms.input.primary,
+  },
+  placeholderText: {
+    color: Colors.primary.s300,
+  },
+  iconWrapper: {
+    left: -45,
+    width: Sizing.x40,
+    height: Sizing.x40,
+  },
+  icon: {
+    width: Sizing.x35,
+    height: Sizing.x35,
   },
 });
