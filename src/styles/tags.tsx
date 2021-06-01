b14a742c639d34340fe3e731dd2ad3caad3ee3dc
Piotr Napierala <piotr.napierala94@gmail.com>
2021-06-01 21:32:19 +0200

    feat(onboarding): Add predefined tags styles

---
+++ src/styles/tags.tsx
@@ -0,0 +1,21 @@
+import { ViewStyle, TextStyle } from "react-native";
+
+import * as Colors from "./colors";
+import * as Sizing from "./sizing";
+import * as Outlines from "./outlines";
+import * as Typography from "./typography";
+
+type TagContainer = "small" | "medium" | "large";
+export const tagContainer: Record<TagContainer, ViewStyle> = {
+  small: {
+    borderRadius: Outlines.borderRadius.large,
+  },
+};
+
+type TagHeader = "small" | "medium" | "large";
+export const tagHeader: Record<TagHeader, TextStyle> = {
+  small: {
+    ...Typography.header.x10,
+    fontSize: Sizing.x12,
+  },
+};
