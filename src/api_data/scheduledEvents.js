b90c897225e313df9e5de4d80187c3c376467c95
Piotr Napierala <piotr.napierala94@gmail.com>
2021-06-17 22:32:12 +0200

    feat(calendar): add more dummy data

--- src/api_data/scheduledEvents.js
+++ src/api_data/scheduledEvents.js
@@ -6,12 +6,48 @@ export const scheduledEvents = [
         month: "May",
         days: [
           {
-            day: 15,
+            day: 13,
             scheduledEvents: [
               {
                 title: "My Test Event",
+                index: 0,
+                fromTime: 1620910000000,
+                toTime: 1620913600000,
+                organizer: "@bob_marley",
+                description: "A very important event that I cannot miss!",
+                participants: [
+                  "piotr.napierala94@gmail.com",
+                  "john@travolta.com",
+                ],
+              },
+            ],
+          },
+          {
+            day: 14,
+            scheduledEvents: [
+              {
+                title: "My Test Event 2",
+                index: 1,
+                fromTime: 1621010000000,
+                toTime: 1621013600000,
+                organizer: "@bob_marley",
+                description: "A very important event that I cannot miss!",
+                participants: [
+                  "piotr.napierala94@gmail.com",
+                  "john@travolta.com",
+                ],
+              },
+            ],
+          },
+          {
+            day: 15,
+            scheduledEvents: [
+              {
+                title: "My Test Event 3",
+                index: 2,
                 fromTime: 1621083600000,
                 toTime: 1621087200000,
+                organizer: "@bob_marley",
                 description: "A very important event that I cannot miss!",
                 participants: [
                   "piotr.napierala94@gmail.com",
@@ -24,9 +60,11 @@ export const scheduledEvents = [
             day: 16,
             scheduledEvents: [
               {
-                title: "My Test Event 2",
+                title: "My Test Event 4",
+                index: 3,
                 fromTime: 1621166400000,
                 toTime: 1621168200000,
+                organizer: "@bob_marley",
                 description: "A very important event that I cannot miss!",
                 participants: [
                   "piotr.napierala94@gmail.com",
