c1cc5bb88b6e346e3857ac9aae16d9c56e44468d
Piotr Napierala <piotr.napierala94@gmail.com>
2021-06-19 15:48:11 +0200

    Create dummy transactions data

---
+++ src/api_data/transactions.js
@@ -0,0 +1,23 @@
+export const transactions = [
+  {
+    sender: "DocOcatavius.md",
+    recipient: "JoeStardom1",
+    date: 1619906400000,
+    oldUtxo: 102,
+    newUtxo: 52,
+  },
+  {
+    sender: "JoeStardom1",
+    recipient: "DocOcatavius.md",
+    date: 1619820000000,
+    oldUtxo: 52,
+    newUtxo: 102,
+  },
+  {
+    sender: "DocOcatavius.md",
+    recipient: "JoeStardom1",
+    date: 1619820000000,
+    oldUtxo: 102,
+    newUtxo: 52,
+  },
+];
