ee736204c5da3826359b5d39c5bd5a8c6a629df7
Piotr Napierala <piotr.napierala94@gmail.com>
2021-06-24 21:15:15 +0200

    Chore: add .nvmrc & add more dummy_data

--- src/api_data/browseFeatured.js
+++ src/api_data/browseFeatured.js
@@ -21,6 +21,24 @@ export const browseFeatured = [
         backgroundImage: "assets/images/clothes.jpg",
         backgroundColor: "#9D174D",
       },
+      {
+        type: "background-image",
+        title: "Investing",
+        backgroundImage: "assets/images/investing.jpg",
+        backgroundColor: "#3730A3",
+      },
+      {
+        type: "background-image",
+        title: "Developing",
+        backgroundImage: "assets/images/developing.jpg",
+        backgroundColor: "#5B21B6",
+      },
+      {
+        type: "background-image",
+        title: "Fashion",
+        backgroundImage: "assets/images/clothes.jpg",
+        backgroundColor: "#9D174D",
+      },
     ],
   },
   {
@@ -42,14 +60,44 @@ export const browseFeatured = [
         alias: "SpaceHotdog",
         occupation: "Software Engineer",
         description:
-          "Lorem ipsum dolor sit amet, consectetur  elit. Vestibulum venenatis quam sem, eget bibendum...",
+          "Lorem ipsum dolor sit amet, consectetur  elit. Vestibulum venenatis quam consectetur  elit. Vestibulum venenatis quam sem, eget bibendum...",
         backgroundImage: "assets/images/profilePicThree.png",
+        backgroundColor: "#E0E7FF",
+        bodyColor: "#9D174D",
+      },
+      {
+        type: "profile",
+        alias: "CryptoJunky1",
+        occupation: "Software Engineer",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur  elit. Vestibulum venenatis quam sem, eget bibendum...",
+        backgroundImage: "assets/images/profilePicFour.png",
+        backgroundColor: "#EEDDEE",
+        bodyColor: "#065F46",
+      },
+      {
+        type: "profile",
+        alias: "JoeStardom1",
+        occupation: "Fashion Design",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur  elit. Vestibulum venenatis quam sem, eget bibendum. Vestibulum venenatis quam sem e..",
+        backgroundImage: "assets/images/profilePicTwo.png",
         backgroundColor: "#FCE7F3",
         bodyColor: "#9D174D",
       },
       {
         type: "profile",
-        alias: "Crypto junky",
+        alias: "SpaceHotdog",
+        occupation: "Software Engineer",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur  elit. Vestibulum venenatis quam sem, eget bibendum...",
+        backgroundImage: "assets/images/profilePicThree.png",
+        backgroundColor: "#E0E7FF",
+        bodyColor: "#9D174D",
+      },
+      {
+        type: "profile",
+        alias: "CryptoJunky1",
         occupation: "Software Engineer",
         description:
           "Lorem ipsum dolor sit amet, consectetur  elit. Vestibulum venenatis quam sem, eget bibendum...",
@@ -63,6 +111,66 @@ export const browseFeatured = [
     title: "Available This Month",
     type: "categories",
     items: [
+      {
+        type: "background-image",
+        title: "Investing",
+        backgroundImage: "assets/images/investing.jpg",
+        backgroundColor: "#3730A3",
+      },
+      {
+        type: "background-image",
+        title: "Developing",
+        backgroundImage: "assets/images/developing.jpg",
+        backgroundColor: "#5B21B6",
+      },
+      {
+        type: "background-image",
+        title: "Fashion",
+        backgroundImage: "assets/images/clothes.jpg",
+        backgroundColor: "#9D174D",
+      },
+      {
+        type: "background-image",
+        title: "Health",
+        backgroundImage: "assets/images/health.png",
+        backgroundColor: "#3730A3",
+      },
+      {
+        type: "background-image",
+        title: "Lawyer",
+        backgroundImage: "assets/images/lawyer.png",
+        backgroundColor: "#9D174D",
+      },
+      {
+        type: "background-image",
+        title: "Fashion",
+        backgroundImage: "assets/images/fashio.png",
+        backgroundColor: "#92400E",
+      },
+    ],
+  },
+  {
+    title: "New This Week",
+    type: "categories",
+    items: [
+      {
+        type: "background-image",
+        title: "Investing",
+        backgroundImage: "assets/images/investing.jpg",
+        backgroundColor: "#3730A3",
+      },
+      {
+        type: "background-image",
+        title: "Developing",
+        backgroundImage: "assets/images/developing.jpg",
+        backgroundColor: "#5B21B6",
+      },
+      {
+        type: "background-image",
+        title: "Fashion",
+        backgroundImage: "assets/images/clothes.jpg",
+        backgroundColor: "#9D174D",
+      },
       {
         type: "background-image",
         title: "Health",
