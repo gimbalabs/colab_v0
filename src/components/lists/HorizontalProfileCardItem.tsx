359d0944bf94cbdac46bb06248f93d98984fe9a3
Piotr Napierala <piotr.napierala94@gmail.com>
2021-06-24 21:21:23 +0200

    [browse-screen] add HorizontalProfileCardItem

--- src/components/lists/HorizontalProfileCardItem.tsx
+++ src/components/lists/HorizontalProfileCardItem.tsx
@@ -1,5 +1,10 @@
 import * as React from "react";
-import { StyleSheet, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+
+import profilePic_1 from "assets/images/profilePicTwo.png";
+import profilePic_2 from "assets/images/profilePicThree.png";
+import profilePic_3 from "assets/images/profilePicFour.png";
+import { Sizing, Outlines, Typography, Colors } from "styles/index";
 
 export interface HorizontalProfileCardItemProps {
   item: any;
@@ -8,9 +13,82 @@ export interface HorizontalProfileCardItemProps {
 export const HorizontalProfileCardItem = ({
   item,
 }: HorizontalProfileCardItemProps) => {
-  return <View style={styles.container}></View>;
+  const randomPlaceholder = React.useCallback(() => {
+    var images = [profilePic_1, profilePic_2, profilePic_3];
+
+    return images[Math.floor(Math.random() * images.length)];
+  }, [item]);
+
+  return (
+    <View style={[styles.container, { backgroundColor: item.backgroundColor }]}>
+      <View style={styles.header}>
+        <ImageBackground
+          source={randomPlaceholder()}
+          imageStyle={styles.image}
+          style={styles.imageView}
+        />
+        <View style={styles.headerContent}>
+          <Text style={styles.headerText}>{item.alias}</Text>
+          <Text
+            numberOfLines={1}
+            lineBreakMode="tail"
+            style={styles.headerSubText}>
+            {item.occupation}
+          </Text>
+        </View>
+      </View>
+      <View style={styles.body}>
+        <Text
+          numberOfLines={5}
+          lineBreakMode="tail"
+          style={[styles.bodyText, { color: item.bodyColor }]}>
+          {item.description}
+        </Text>
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    height: 155,
+    width: 155,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    marginRight: Sizing.x12,
+    padding: Sizing.x15,
+    borderRadius: Outlines.borderRadius.base,
+    ...Outlines.shadow.lifted,
+  },
+  image: {
+    borderRadius: Outlines.borderRadius.max,
+  },
+  imageView: {
+    width: Sizing.x50,
+    height: Sizing.x50,
+    ...Outlines.shadow.lifted,
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  headerContent: {
+    marginLeft: Sizing.x8,
+  },
+  headerText: {
+    ...Typography.header.x10,
+    color: Colors.primary.s800,
+  },
+  headerSubText: {
+    ...Typography.subHeader.x5,
+  },
+  body: {
+    marginTop: Sizing.x5,
+    alignSelf: "flex-start",
+  },
+  bodyText: {
+    fontSize: Sizing.x12,
+    lineHeight: 15,
+    fontFamily: "Roboto-Regular",
+  },
 });
