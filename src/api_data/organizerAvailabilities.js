19fabea74151ad7e611970ae2d0f169d07517e3f
Piotr Napierala <piotr.napierala94@gmail.com>
2021-06-29 22:11:24 +0200

    Add dummy organizer availability

---
+++ src/api_data/organizerAvailabilities.js
@@ -0,0 +1,23 @@
+export const organizerAvailabilities = {
+  years: [
+    {
+      year: 2021,
+      months: [
+        {
+          month: "May",
+          availableDays: [
+            {
+              day: 2,
+              limited: "none",
+              timeSlots: {
+                from: 1619856000000,
+                to: 1619892000000,
+              },
+              bookedSlots: [1619870400000, 1619872200000],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
