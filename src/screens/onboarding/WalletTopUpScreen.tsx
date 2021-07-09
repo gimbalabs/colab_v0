987958e3fa0c83058de7c30a1401fc37e62604d5
Piotr Napierala <piotr.napierala94@gmail.com>
2021-07-09 22:06:14 +0200

    [screens] add WalletSetUpModal, navigation & clipboard to WalletTopUpScreen

--- src/screens/onboarding/WalletTopUpScreen.tsx
+++ src/screens/onboarding/WalletTopUpScreen.tsx
@@ -8,8 +8,10 @@ import {
   Platform,
   ScrollView,
   SafeAreaView,
+  useWindowDimensions,
 } from "react-native";
 
+import ViewPager from "react-native-pager-view";
 import QRCode from "react-native-qrcode-svg";
 import { CustomPlainInput } from "components/forms/CustomPlainInput";
 import { DuplicateIcon, LeftArrowIcon } from "icons/index";
@@ -18,18 +20,36 @@ import { appContext } from "contexts/contextApi";
 import { FullWidthButton } from "components/buttons/fullWidthButton";
 import { BodyText } from "components/rnWrappers/bodyText";
 import { ProfileContext } from "contexts/profileContext";
+import { WalletSetUpModal } from "components/modals/walletSetUpModal";
+import { useNavigation } from "@react-navigation/native";
+import * as Clipboard from "expo-clipboard";
+import { CopyMessage } from "components/popups/copyMessage";
 
-export interface WalletTopUpScreenProps {}
+export interface WalletTopUpScreenProps {
+  pagerRef?: React.RefObject<ViewPager>;
+  navigation?: any;
+  route?: any;
+}
 
-export const WalletTopUpScreen = ({ navigation, route }) => {
-  const { colorScheme } = appContext();
+export const WalletTopUpScreen = ({
+  route,
+  pagerRef,
+}: WalletTopUpScreenProps) => {
+  const navigation = useNavigation();
+  var { colorScheme } = appContext();
+  const [isLightMode, setIsLightMode] = React.useState<boolean>(false);
   const [address, setAddress] = React.useState<string>("");
   const [amount, setAmount] = React.useState<string>("");
+  const [copyMsgActive, setCopyMsgActive] = React.useState<boolean>(false);
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
-  const { walletBalance, setWalletBalance } = React.useContext(ProfileContext);
+  const [isVisibleModal, setIsVisibleModal] = React.useState<boolean>(false);
+  const { hasSyncedWallet, walletBalance, setWalletBalance } = React.useContext(
+    ProfileContext
+  );
+  const windowWidth = useWindowDimensions().width;
 
+  const isOnboardingScreen = pagerRef?.current instanceof ViewPager;
   const isBookingScreen = route != null && route.params?.isBookingScreen;
-  const isLightMode = colorScheme === "light";
   const isDisabled = !address || !amount;
 
   const onTextChangeCallback = (value: any) => setAddress(value);
@@ -41,110 +61,163 @@ export const WalletTopUpScreen = ({ navigation, route }) => {
     new Promise((res) => setTimeout(res, ms));
 
   const processPayment = async () => {
+    // if user hasn't synced any wallet yet:
+    if (!hasSyncedWallet) return setIsVisibleModal(true);
+
     setIsLoading(true);
     //@TODO change in production
     await wait(2000);
     setWalletBalance(Number(walletBalance) + Number(amount));
     setIsLoading(false);
+
+    // this means we are on the onboarding screen
+    if (
+      isOnboardingScreen ||
+      (route != null && route.params?.fromScreen != null)
+    ) {
+      navigation.navigate("Deposit Successful", {
+        fromScreen: route.params.fromScreen,
+      });
+    }
     navigation.navigate("Deposit Successful", { isBookingWalletTopUp: true });
   };
 
+  const hideModal = () => setIsVisibleModal(false);
+
+  const WalletTopUpModal = React.useMemo(
+    () => (
+      <View>
+        <WalletSetUpModal hideModal={hideModal} isVisible={isVisibleModal} />
+      </View>
+    ),
+    [isVisibleModal]
+  );
+
+  const onCopyPress = () => {
+    Clipboard.default.setString(address);
+    setCopyMsgActive(true);
+    setTimeout(() => setCopyMsgActive(false), 2000);
+  };
+
+  React.useEffect(() => {
+    if (pagerRef?.current instanceof ViewPager) {
+      setIsLightMode(false);
+    } else {
+      setIsLightMode(colorScheme === "light");
+    }
+  }, [colorScheme]);
+
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        backgroundColor: isLightMode
-          ? Colors.primary.neutral
-          : Colors.primary.s800,
-      }}>
-      <KeyboardAvoidingView
-        keyboardVerticalOffset={20}
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-        style={[isLightMode ? styles.container_light : styles.container_dark]}>
-        <ScrollView
-          keyboardShouldPersistTaps="handled"
-          showsVerticalScrollIndicator={false}
-          style={styles.scrollView}>
-          <View style={styles.navigation}>
-            <Pressable onPress={onBackNavigationPress} hitSlop={10}>
-              <LeftArrowIcon
-                width={24}
-                height={24}
-                color={
-                  isLightMode ? Colors.primary.s600 : Colors.primary.neutral
-                }
-              />
-            </Pressable>
-          </View>
-          <View style={styles.header}>
-            <Text
-              style={[
-                isLightMode ? styles.headerText_light : styles.headerText_dark,
-              ]}>
-              {isBookingScreen ? "Add funds" : "Set up your wallet"}
-            </Text>
-            {isBookingScreen ? (
-              <BodyText colors={[Colors.primary.s600, Colors.primary.neutral]}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Vestibulum venenatis quam sem, eget bibendum lorem convallis et.
-                Donec velit ante, efficitur at ante eu, consequat hendrerit
-                augue. Vivamus quis eros ex
-              </BodyText>
+    <>
+      <SafeAreaView
+        style={{
+          flex: 1,
+          backgroundColor: isLightMode
+            ? Colors.primary.neutral
+            : Colors.primary.s800,
+        }}>
+        <KeyboardAvoidingView
+          keyboardVerticalOffset={40}
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style={[
+            isLightMode ? styles.container_light : styles.container_dark,
+          ]}>
+          <ScrollView
+            keyboardShouldPersistTaps="handled"
+            showsVerticalScrollIndicator={false}
+            style={styles.scrollView}>
+            {!isOnboardingScreen ? (
+              <View style={styles.navigation}>
+                <Pressable onPress={onBackNavigationPress} hitSlop={10}>
+                  <LeftArrowIcon
+                    width={24}
+                    height={24}
+                    color={
+                      isLightMode ? Colors.primary.s600 : Colors.primary.neutral
+                    }
+                  />
+                </Pressable>
+              </View>
             ) : (
+              <View style={{ marginTop: Sizing.x20 }} />
+            )}
+            <View style={styles.header}>
               <Text
                 style={[
                   isLightMode
-                    ? styles.subHeaderText_light
-                    : styles.subHeaderText_dark,
+                    ? styles.headerText_light
+                    : styles.headerText_dark,
                 ]}>
-                Deposit /withdrawl ADA into your wallet to make payments, and
-                receive payments.
+                {isBookingScreen ? "Add funds" : "Set up your wallet"}
               </Text>
-            )}
-          </View>
-          <View style={styles.main}>
-            <View style={styles.qrCodeContainer}>
-              <QRCode
-                value="https://gimbalabs.com"
-                size={220}
-                backgroundColor={Colors.primary.neutral}
+              {isBookingScreen ? (
+                <BodyText
+                  colors={[Colors.primary.s600, Colors.primary.neutral]}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Vestibulum venenatis quam sem, eget bibendum lorem convallis
+                  et. Donec velit ante, efficitur at ante eu, consequat
+                  hendrerit augue. Vivamus quis eros ex
+                </BodyText>
+              ) : (
+                <Text
+                  style={[
+                    isLightMode
+                      ? styles.subHeaderText_light
+                      : styles.subHeaderText_dark,
+                  ]}>
+                  Deposit /withdrawl ADA into your wallet to make payments, and
+                  receive payments.
+                </Text>
+              )}
+            </View>
+            <View style={styles.main}>
+              <View style={styles.qrCodeContainer}>
+                <QRCode
+                  value="https://gimbalabs.com"
+                  size={windowWidth / 2}
+                  backgroundColor={Colors.primary.neutral}
+                />
+              </View>
+            </View>
+            <CustomPlainInput
+              label="Address"
+              placeholder="addr9czf30t9dzbdsxe79a2vtf8io"
+              icon={DuplicateIcon}
+              styles={Object.assign(
+                styles,
+                isLightMode ? formStyleLight : formStyleDark
+              )}
+              onChangeCallback={onTextChangeCallback}
+              onPressHandler={onCopyPress}
+              customChild={<CopyMessage isActive={copyMsgActive} />}
+            />
+            <CustomPlainInput
+              label="Amount"
+              placeholder="50 ₳"
+              styles={Object.assign(
+                styles,
+                isLightMode ? formStyleLight : formStyleDark
+              )}
+              keyboardType="numeric"
+              onChangeCallback={onAmountChangeCallback}
+              onPressHandler={() => {}}
+            />
+            <View style={styles.buttonWrapper}>
+              <FullWidthButton
+                onPressCallback={processPayment}
+                text={"Deposit"}
+                colorScheme={colorScheme}
+                disabled={isDisabled}
+                loadingIndicator={isLoading}
+                buttonType={!isLightMode ? "transparent" : "filled"}
+                lightMode={isLightMode}
               />
             </View>
-          </View>
-          <CustomPlainInput
-            label="Address"
-            placeholder="addr9czf30t9dzbdsxe79a2vtf8io"
-            icon={DuplicateIcon}
-            styles={Object.assign(
-              styles,
-              isLightMode ? formStyleLight : formStyleDark
-            )}
-            onChangeCallback={onTextChangeCallback}
-            onPressHandler={() => {}}
-          />
-          <CustomPlainInput
-            label="Amount"
-            placeholder="50 ₳"
-            styles={Object.assign(
-              styles,
-              isLightMode ? formStyleLight : formStyleDark
-            )}
-            keyboardType="numeric"
-            onChangeCallback={onAmountChangeCallback}
-            onPressHandler={() => {}}
-          />
-          <View style={styles.buttonWrapper}>
-            <FullWidthButton
-              onPressCallback={processPayment}
-              text={"Deposit"}
-              colorScheme={colorScheme}
-              disabled={isDisabled}
-              loadingIndicator={isLoading}
-            />
-          </View>
-        </ScrollView>
-      </KeyboardAvoidingView>
-    </SafeAreaView>
+          </ScrollView>
+        </KeyboardAvoidingView>
+        {isVisibleModal && WalletTopUpModal}
+      </SafeAreaView>
+    </>
   );
 };
 
@@ -165,12 +238,12 @@ const styles = StyleSheet.create({
     height: "100%",
   },
   headerText_light: {
-    ...Typography.header.x70,
+    ...Typography.header.x65,
     color: Colors.primary.s800,
     marginBottom: Sizing.x5,
   },
   headerText_dark: {
-    ...Typography.header.x70,
+    ...Typography.header.x65,
     color: Colors.primary.neutral,
     marginBottom: Sizing.x5,
   },
@@ -180,17 +253,17 @@ const styles = StyleSheet.create({
     marginVertical: Sizing.x25,
   },
   qrCodeContainer: {
-    padding: Sizing.x20,
+    padding: Sizing.x25,
     backgroundColor: Colors.primary.neutral,
     borderRadius: Outlines.borderRadius.base,
   },
   subHeaderText_light: {
-    ...Typography.subHeader.x40,
+    ...Typography.subHeader.x35,
     fontFamily: "Roboto-Regular",
     color: Colors.primary.s600,
   },
   subHeaderText_dark: {
-    ...Typography.subHeader.x40,
+    ...Typography.subHeader.x35,
     fontFamily: "Roboto-Regular",
     color: Colors.primary.neutral,
   },
@@ -200,7 +273,7 @@ const styles = StyleSheet.create({
     marginVertical: Sizing.x15,
   },
   buttonWrapper: {
-    marginVertical: Sizing.x20,
+    marginVertical: Sizing.x12,
   },
   /**
    * styles passed as prop to CustomPlainInput
@@ -211,7 +284,7 @@ const styles = StyleSheet.create({
   },
   labelContainer: {
     width: "100%",
-    paddingHorizontal: Sizing.x12,
+    paddingHorizontal: Sizing.x15,
     paddingBottom: Sizing.x2,
   },
   textInputWrapper: {
