fa115d9edcc67181b5dab537e055e0ae24456551
Piotr Napierala <piotr.napierala94@gmail.com>
2021-07-14 22:45:06 +0200

    [fake-data] add organizer events

---
+++ src/api_data/events.js
@@ -0,0 +1,16 @@
+export const events = [
+  {
+    title: "What to wear this Summer",
+    fromDate: 1627768800000,
+    toDate: 1630447200000,
+    image: "Summer",
+    color: "#0F4871",
+  },
+  {
+    title: "What to wear this Fall",
+    fromDate: 1633816800000,
+    toDate: 1635112800000,
+    image: "Fall", // should this be a url?
+    color: "#831843",
+  },
+];
