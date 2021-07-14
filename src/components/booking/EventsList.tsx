5af29225f198f5db85941ca28db9c048bca5fb83
Piotr Napierala <piotr.napierala94@gmail.com>
2021-07-14 22:46:42 +0200

    [components/booking] add flatList to EventsList

--- src/components/booking/EventsList.tsx
+++ src/components/booking/EventsList.tsx
@@ -1,16 +1,46 @@
 import * as React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+
+//@ts-ignore
+import SummerImg from "../../assets/images/summer.jpeg";
+//@ts-ignore
+import FallImg from "../../assets/images/fall.jpg";
+
+import { events } from "../../api_data/events";
+import { EventsListCard } from "./EventsListCard";
 
 export interface EventsListProps {}
 
 export const EventsList = ({}: EventsListProps) => {
+  const renderEventCard = ({ item }: any) => {
+    const { title, fromDate, toDate, image, color } = item;
+
+    const imageSrc = image === "Fall" ? FallImg : SummerImg;
+
+    return (
+      <EventsListCard
+        title={title}
+        fromDate={fromDate}
+        toDate={toDate}
+        image={imageSrc}
+        color={color}
+      />
+    );
+  };
+
+  const keyExtractor = (item, index) => item.title + index;
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-    </View>
+    <FlatList
+      style={{ flex: 1 }}
+      data={[...events, ...events]}
+      renderItem={renderEventCard}
+      keyExtractor={keyExtractor}
+      showsVerticalScrollIndicator={false}
+    />
   );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1 },
+  container: { flex: 1, width: "100%" },
 });
