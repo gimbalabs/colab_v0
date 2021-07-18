2ad5047881d22538ee1ffdacfa756502d4626d48
Piotr Napierala <piotr.napierala94@gmail.com>
2021-07-18 14:12:29 +0200

    [fake-data] modify dates of customScheduledEvents

--- src/api_data/customScheduledEvents.js
+++ src/api_data/customScheduledEvents.js
@@ -3,64 +3,86 @@ export const customScheduledEvents = [
     year: 2021,
     months: [
       {
-        month: "May",
+        month: "July",
         totalNumOfEvents: 4,
         days: [
-          {
-            day: 15,
-            scheduledEvents: [
-              {
-                title: "My Test Event",
-                fromTime: 1621074600000,
-                toTime: 1621078200000,
-                organizer: "@bob_marley",
-                description: "A very important event that I cannot miss!",
-                participants: [
-                  "piotr.napierala94@gmail.com",
-                  "john@travolta.com",
-                ],
-              },
-            ],
-          },
-          {
-            day: 16,
-            scheduledEvents: [
-              {
-                title: "My Test Event",
-                fromTime: 1621173600000,
-                toTime: 1621177200000,
-                organizer: "@bob_marley",
-                description: "A very important event that I cannot miss!",
-                participants: [
-                  "piotr.napierala94@gmail.com",
-                  "john@travolta.com",
-                ],
-              },
-            ],
-          },
-          {
-            day: 17,
-            scheduledEvents: [
-              {
-                title: "My Test Event",
-                fromTime: 1621238400000,
-                toTime: 1621263600000,
-                organizer: "@bob_marley",
-                description: "A very important event that I cannot miss!",
-                participants: [
-                  "piotr.napierala94@gmail.com",
-                  "john@travolta.com",
-                ],
-              },
-            ],
-          },
           {
             day: 18,
             scheduledEvents: [
               {
+                title: "My Test Event",
+                fromTime: "2021-07-18T13:00:00.000Z",
+                toTime: "2021-07-18T15:00:00.000Z",
+                organizer: "@bob_marley",
+                description: "A very important event that I cannot miss!",
+                participants: [
+                  "piotr.napierala94@gmail.com",
+                  "john@travolta.com",
+                ],
+              },
+              {
+                title: "My Test Event",
+                fromTime: "2021-07-18T15:00:00.000Z",
+                toTime: "2021-07-18T15:30:00.000Z",
+                organizer: "@bob_marley",
+                description: "A very important event that I cannot miss!",
+                participants: [
+                  "piotr.napierala94@gmail.com",
+                  "john@travolta.com",
+                ],
+              },
+              {
+                title: "My Test Event",
+                fromTime: "2021-07-18T15:30:00.000Z",
+                toTime: "2021-07-18T16:00:00.000Z",
+                organizer: "@bob_marley",
+                description: "A very important event that I cannot miss!",
+                participants: [
+                  "piotr.napierala94@gmail.com",
+                  "john@travolta.com",
+                ],
+              },
+            ],
+          },
+          {
+            day: 19,
+            scheduledEvents: [
+              {
+                title: "My Test Event",
+                fromTime: "2021-07-19T15:00:00.000Z",
+                toTime: "2021-07-19T16:30:00.000Z",
+                organizer: "@bob_marley",
+                description: "A very important event that I cannot miss!",
+                participants: [
+                  "piotr.napierala94@gmail.com",
+                  "john@travolta.com",
+                ],
+              },
+            ],
+          },
+          {
+            day: 20,
+            scheduledEvents: [
+              {
+                title: "My Test Event",
+                fromTime: "2021-07-20T12:00:00.000Z",
+                toTime: "2021-07-20T13:00:00.000Z",
+                organizer: "@bob_marley",
+                description: "A very important event that I cannot miss!",
+                participants: [
+                  "piotr.napierala94@gmail.com",
+                  "john@travolta.com",
+                ],
+              },
+            ],
+          },
+          {
+            day: 21,
+            scheduledEvents: [
+              {
                 title: "My Test Event 3",
-                fromTime: 1621346400000,
-                toTime: 1621350000000,
+                fromTime: "2021-07-21T12:00:00.000Z",
+                toTime: "2021-07-21T13:00:00.000Z",
                 organizer: "@bob_marley",
                 description: "A very important event that I cannot miss!",
                 participants: [
