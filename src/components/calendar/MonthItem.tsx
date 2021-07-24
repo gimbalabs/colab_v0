3334f5432c738f3caf93eaaabb540f9c89f80fcf
Piotr Napierala <piotr.napierala94@gmail.com>
2021-07-24 22:35:03 +0200

    [components/calendar] insert BookingDay & AvailabilityDay into MonthItem

--- src/components/calendar/MonthItem.tsx
+++ src/components/calendar/MonthItem.tsx
@@ -2,13 +2,19 @@ import * as React from "react";
 
 import { LayoutRectangle } from "react-native";
 import { PlaceholderDay } from "./PlaceholderDay";
-import { MonthlyDay } from "./MonthlyDay";
+import { MonthlyDay } from "./days/MonthlyDay";
 import { Month } from "interfaces/myCalendarInterface";
+import { BookingDay } from "./days/BookingDay";
+import { AvailabilityDay } from "./days/AvailabilityDay";
+import { getTime } from "lib/utils";
+import { monthsByName } from "common/types/calendarTypes";
 
 export interface MonthProps extends Month {
   dimensions: LayoutRectangle | null;
   onPlaceholderPress: (direction: string) => void;
   isBookingCalendar?: boolean;
+  isAvailabilityCalendar?: boolean;
+  isNewEventCalendar?: boolean;
   month: string;
 }
 
@@ -18,8 +24,17 @@ export const MonthItem = ({
   days,
   onPlaceholderPress,
   isBookingCalendar,
+  isNewEventCalendar,
 }: MonthProps) => {
   const [activeDay, setActiveDay] = React.useState<number | null>(null);
+  const [availableDays, setAvailableDays] = React.useState<number[]>([]);
+
+  const isSelectedAvailability = React.useCallback(
+    (year, month, number) => {
+      return availableDays.includes(getTime(year, monthsByName[month], number));
+    },
+    [availableDays]
+  );
 
   return (
     <>
@@ -31,19 +46,35 @@ export const MonthItem = ({
             key={`${day.name}-${day.number}`}
             number={day.number}
           />
+        ) : isBookingCalendar ? (
+          <BookingDay
+            month={month}
+            year={year}
+            name={day.name}
+            key={`${month}-${day.name}-${day.number}`}
+            number={day.number}
+            availabilities={day.availabilities}
+            activeDay={activeDay}
+            setActiveDay={setActiveDay}
+          />
+        ) : isNewEventCalendar ? (
+          <AvailabilityDay
+            key={`${month}-${day.name}-${day.number}`}
+            number={day.number}
+            isSelectedDay={isSelectedAvailability(year, month, day.number)}
+            setAvailableDays={setAvailableDays}
+          />
         ) : (
           <MonthlyDay
             month={month}
             year={year}
             name={day.name}
-            key={`${day.name}-${day.number}-${year}`}
+            key={`${month}-${day.name}-${day.number}`}
             number={day.number}
             availabilities={day.availabilities}
             activeDay={activeDay}
             setActiveDay={setActiveDay}
             scheduledEvents={day.scheduledEvents}
-            isLastWeek={day.isLastWeek || false}
-            isBookingCalendar={isBookingCalendar}
           />
         )
       )}
