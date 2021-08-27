21a47ec3e09562ea12aff415186fb80324d0c82f
Piotr Napierala <piotr.napierala94@gmail.com>
2021-08-27 13:34:02 +0200

    [components/modals] new: nativeModal

---
+++ src/components/modals/nativeModal.tsx
@@ -0,0 +1,83 @@
+import * as React from "react";
+import { View, Text, Pressable, StyleSheet } from "react-native";
+
+import ModalSelector from "react-native-modal-selector";
+import { Buttons, Sizing, Typography } from "styles/index";
+
+export interface NativeModalProps {
+  cameraAccessCb: () => any;
+  mediaLibraryCb: () => any;
+  child: React.ReactNode;
+}
+
+export const NativeModal = React.memo(
+  ({ cameraAccessCb, mediaLibraryCb, child }: NativeModalProps) => {
+    const [selector, setSelector] = React.useState<ModalSelector | null>(null);
+
+    const modalData = [
+      {
+        key: 1,
+        testID: "1-take-photo",
+        label: "Take Photo",
+        component: (
+          <Pressable
+            style={Buttons.applyOpacity({
+              alignItems: "center",
+              justifyContent: "center",
+              height: Sizing.x30,
+            })}
+            hitSlop={10}
+            onPress={cameraAccessCb}>
+            <Text
+              style={{
+                ...Typography.subHeader.x25,
+                textAlignVertical: "center",
+                includeFontPadding: true,
+                textAlign: "justify",
+              }}>
+              Take photo
+            </Text>
+          </Pressable>
+        ),
+      },
+      {
+        key: 2,
+        testID: "2-take-photo",
+        label: "Browse...",
+        component: (
+          <Pressable
+            style={Buttons.applyOpacity({
+              alignItems: "center",
+              justifyContent: "center",
+              height: Sizing.x30,
+            })}
+            hitSlop={10}
+            onPress={mediaLibraryCb}>
+            <Text
+              style={{
+                ...Typography.subHeader.x25,
+                textAlignVertical: "center",
+                includeFontPadding: true,
+                textAlign: "justify",
+              }}>
+              Browse...
+            </Text>
+          </Pressable>
+        ),
+      },
+    ];
+
+    return (
+      <>
+        {/* @ts-ignore) */}
+        <ModalSelector
+          closeOnChange={false}
+          cancelText="Cancel"
+          ref={(_selector) => setSelector(_selector)}
+          data={modalData}>
+          {child}
+        </ModalSelector>
+      </>
+    );
+  }
+);
