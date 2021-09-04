188ffaf2a9088bb323d450c3e680adefac861ff6
Piotr Napierala <piotr.napierala94@gmail.com>
2021-09-04 22:39:34 +0200

    [lib] add signChallange fn

--- src/lib/tweetnacl.ts
+++ src/lib/tweetnacl.ts
@@ -1,14 +1,12 @@
-//@ts-ignore
 import nacl from "tweet-nacl-react-native-expo";
+import base64 from "base64-js";
 
 interface KeyPair {
   publicKey: Uint8Array;
   secretKey: Uint8Array;
 }
 
-export const generateKeyPair = async (
-  seed?: string
-): Promise<KeyPair | void> => {
+export const generateKeyPair = async (): Promise<KeyPair | void> => {
   try {
     const keypair = await nacl.sign.keyPair();
     return keypair;
@@ -17,9 +15,18 @@ export const generateKeyPair = async (
   }
 };
 
-export const signChallenge = async (privateKey: string, challenge: string) => {
+export const signChallenge = async (
+  challenge: string | Uint8Array,
+  secretKey: string | Uint8Array
+): Promise<string | void> => {
+  // convert to Uint8Array
+  if (typeof challenge === "string" && typeof secretKey === "string") {
+    challenge = base64.toByteArray(challenge);
+    secretKey = base64.toByteArray(secretKey);
+  }
+
   try {
-    const signedChallenge = await nacl.sign(challenge, privateKey);
+    const signedChallenge = await nacl.sign.detached(challenge, secretKey);
     return signedChallenge;
   } catch (e) {
     console.error(e);
