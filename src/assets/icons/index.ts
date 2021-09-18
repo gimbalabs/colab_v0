573ec794474d93253678a7ab902a597902166f2a
Piotr Napierala <piotr.napierala94@gmail.com>
2021-09-19 01:02:54 +0200

    [components/modals] finish errorModal

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -14,6 +14,7 @@ import { DotIcon } from "./dot";
 import { DownIcon } from "./down";
 import { DuplicateIcon } from "./duplicate";
 import { EditIcon } from "./edit";
+import { ErrorIcon } from "./error";
 import { EyeIcon } from "./eye";
 import { EyeOffIcon } from "./eyeOff";
 import { HearthIcon } from "./hearth";
@@ -61,6 +62,7 @@ export {
   LightBulbIcon,
   RightArrowIcon,
   EditIcon,
+  ErrorIcon,
   EyeIcon,
   EyeOffIcon,
   IdeaIcon,
--- src/components/modals/errorModal.tsx
+++ src/components/modals/errorModal.tsx
@@ -1,24 +1,71 @@
+import { ErrorIcon } from "assets/icons";
+import { SubHeaderText } from "components/rnWrappers/subHeaderText";
 import * as React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet, useWindowDimensions } from "react-native";
 
 import Modal from "react-native-modal";
-import { Sizing } from "styles/index";
+import { Colors, Outlines, Sizing } from "styles/index";
 
 export interface ErrorModalProps {
   isModalVisible: boolean;
 }
 
 export const ErrorModal = ({ isModalVisible }: ErrorModalProps) => {
+  const [isVisible, setIsVisible] = React.useState<boolean>(isModalVisible);
+  const { width, height } = useWindowDimensions();
+
+  React.useEffect(() => {
+    setIsVisible(isModalVisible);
+  }, [isModalVisible]);
+
   return (
-    <Modal isVisible={isModalVisible} style={styles.modal}>
-      <Text>Hello World</Text>
+    <Modal
+      animationIn={"slideInDown"}
+      animationInTiming={200}
+      animationOut={"slideInUp"}
+      animationOutTiming={200}
+      isVisible={isVisible}
+      onSwipeCancel={() => setIsVisible(false)}
+      deviceWidth={width}
+      deviceHeight={height}
+      hasBackdrop={false}
+      coverScreen={false}
+      swipeDirection="up"
+      onSwipeComplete={() => setIsVisible(false)}
+      style={styles.modal}>
+      <View style={styles.main}>
+        <ErrorIcon
+          stroke={Colors.primary.neutral}
+          width={Sizing.x60}
+          height={Sizing.x60}
+          strokeWidth={1.5}
+        />
+        <SubHeaderText customStyle={styles.text}>
+          A user with a given user name already exists. Please choose a
+          different one.
+        </SubHeaderText>
+      </View>
     </Modal>
   );
 };
 
 const styles = StyleSheet.create({
   modal: {
-    height: Sizing.x25,
+    margin: 0,
+  },
+  main: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    height: Sizing.x80,
     width: "100%",
+    marginBottom: "auto",
+    backgroundColor: Colors.danger.s300,
+    borderBottomLeftRadius: Outlines.borderRadius.base,
+    borderBottomRightRadius: Outlines.borderRadius.base,
+  },
+  text: {
+    flexWrap: "wrap",
+    width: "80%",
   },
 });
