bf7bfd9f5d3b448b88af6526b5fd3c1f690f6699
Piotr Napierala <piotr.napierala94@gmail.com>
2021-09-20 00:44:07 +0200

    Fix double context in NavigationScreens

--- src/tabs/NavigationScreens.tsx
+++ src/tabs/NavigationScreens.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import { HomeScreen } from "screens/index";
-import { ProfileContextProvider } from "contexts/profileContext";
 import { OrganizerTabParamList } from "common/types/navigationTypes";
 import { WalletScreen } from "screens/index";
 import { Calendar } from "containers/MyCalendar";
@@ -18,21 +17,19 @@ export const NavigationScreens = () => {
   const { accountType } = appContext();
 
   return (
-    <ProfileContextProvider>
-      <NavigationTabs.Navigator
-        //@ts-ignore
-        tabBar={(props) => <NavigationTabBar {...props} />}>
-        <NavigationTabs.Screen
-          name="Home"
-          component={
-            accountType === "attendee" ? HomeScreen : OrganizerHomeScreenStack
-          }
-        />
-        <NavigationTabs.Screen name="Browse" component={BrowseScreensStack} />
-        <NavigationTabs.Screen name="Wallet" component={WalletScreen} />
-        <NavigationTabs.Screen name="Availability" component={Calendar} />
-        <NavigationTabs.Screen name="Profile" component={UserProfileScreen} />
-      </NavigationTabs.Navigator>
-    </ProfileContextProvider>
+    <NavigationTabs.Navigator
+      //@ts-ignore
+      tabBar={(props) => <NavigationTabBar {...props} />}>
+      <NavigationTabs.Screen
+        name="Home"
+        component={
+          accountType === "attendee" ? HomeScreen : OrganizerHomeScreenStack
+        }
+      />
+      <NavigationTabs.Screen name="Browse" component={BrowseScreensStack} />
+      <NavigationTabs.Screen name="Wallet" component={WalletScreen} />
+      <NavigationTabs.Screen name="Availability" component={Calendar} />
+      <NavigationTabs.Screen name="Profile" component={UserProfileScreen} />
+    </NavigationTabs.Navigator>
   );
 };
