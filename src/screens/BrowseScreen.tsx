fe882d24788da36407c8bc759f87cd94e1b9f938
Piotr Napierala <piotr.napierala94@gmail.com>
2021-09-27 00:09:02 +0200

    [screens/BrowseScreen] implement searchbar and searchEvents list

--- src/screens/BrowseScreen.tsx
+++ src/screens/BrowseScreen.tsx
@@ -1,15 +1,18 @@
 import * as React from "react";
-import { View, StyleSheet, ActivityIndicator } from "react-native";
+import { View, StyleSheet, ActivityIndicator, Animated } from "react-native";
 
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StackScreenProps } from "@react-navigation/stack";
 import { Colors, Sizing } from "styles/index";
 import { BookingStackParamList } from "common/types/navigationTypes";
 import { appContext } from "contexts/contextApi";
-import { SearchIcon } from "icons/index";
 import { useEventsPagination } from "lib/hooks/useEventsPagination";
 import { EventsList } from "components/booking/EventsList";
 import { SubHeaderText } from "components/rnWrappers/subHeaderText";
+import { applyOpacity } from "../styles/colors";
+import { SearchBar } from "components/searchBar";
+import { useEventsResults } from "lib/hooks/useEventsResults";
+import { EmptyDocumentsIcon } from "assets/icons";
 // import { browseFeatured } from "../api_data/browseFeatured";
 
 export interface BrowseProps
@@ -19,9 +22,39 @@ export interface BrowseProps
 
 export const BrowseScreen = ({ navigation }: BrowseProps) => {
   const { colorScheme } = appContext();
-  const { events, isLoading } = useEventsPagination();
-  const isLightMode = colorScheme !== "dark";
+  const { events, isLoading: isPaginationLoading } = useEventsPagination();
+  const {
+    events: searchEvents,
+    isLoading: isSearchLoading,
+    getEventsBySearchQuery,
+    setEvents,
+  } = useEventsResults();
 
+  const animatedOpacity = React.useRef(new Animated.Value(0)).current;
+  const isLightMode = colorScheme !== "dark";
+  const isLoading = isSearchLoading || isPaginationLoading;
+  const isEmptyEventsList =
+    (searchEvents && !searchEvents.length) || !events.length;
+
+  const onActiveSearch = (active: boolean) => {
+    Animated.timing(animatedOpacity, {
+      useNativeDriver: true,
+      toValue: active ? 1 : 0,
+      duration: 140,
+    }).start();
+  };
+  const onSubmitSearch = (val: string) => {
+    onActiveSearch(false);
+    getEventsBySearchQuery(val);
+  };
+  const onToggleSearchBar = (val: boolean) => {
+    // user hides search bar, show the normal events list
+    if (!val) setEvents(null);
+  };
+
+  /**
+   * Old code for displaying horizontal lists (categories, organizers, etc.)
+   */
   // const renderFeaturedLists = React.useCallback(() => {
   //   return browseFeatured.map((list, index) => (
   // <HorizontalCardsList navigateTo={navigateTo} key={index} list={list} />
@@ -31,27 +64,22 @@ export const BrowseScreen = ({ navigation }: BrowseProps) => {
   // const navigateTo = (params: BookingStackParamList["Available Dates"]) => {
   //   navigation.navigate("Available Dates", params);
   // };
+
   return (
     <SafeAreaView
       style={[isLightMode ? styles.safeArea_light : styles.safeaArea_dark]}>
-      <View style={styles.container}>
-        <View style={styles.searchToolContainer}>
-          <SearchIcon
-            width={24}
-            height={24}
-            stroke={
-              colorScheme === "light"
-                ? Colors.primary.s800
-                : Colors.primary.neutral
-            }
-            strokeWidth={1.6}
-          />
-        </View>
+      <View style={styles.topContainer}>
+        <SearchBar
+          onSubmitSearch={onSubmitSearch}
+          onActiveSearch={onActiveSearch}
+          onToggleSearchBar={onToggleSearchBar}
+        />
       </View>
       <View style={styles.main}>
-        {!!events.length && !isLoading ? (
-          <EventsList />
-        ) : !events.length && isLoading ? (
+        {(events.length && !searchEvents) ||
+        (searchEvents?.length && !isLoading) ? (
+          <EventsList customEvents={searchEvents} />
+        ) : !events.length && !searchEvents && isLoading ? (
           <ActivityIndicator
             animating={true}
             color={isLightMode ? Colors.primary.s800 : Colors.primary.neutral}
@@ -59,13 +87,26 @@ export const BrowseScreen = ({ navigation }: BrowseProps) => {
             style={{ paddingTop: Sizing.x35 }}
           />
         ) : (
-          <View style={styles.noEventsMessage}>
-            <SubHeaderText
-              colors={[Colors.primary.s800, Colors.primary.neutral]}>
-              Nothing yet to show...
-            </SubHeaderText>
-          </View>
+          isEmptyEventsList && (
+            <View style={styles.noEventsMessage}>
+              <EmptyDocumentsIcon width="30%" height="30%" />
+              <SubHeaderText
+                colors={[Colors.primary.s800, Colors.primary.neutral]}>
+                Nothing to show yet...
+              </SubHeaderText>
+            </View>
+          )
         )}
+        <Animated.View
+          pointerEvents="none"
+          style={[
+            styles.overlay,
+            {
+              opacity: animatedOpacity,
+              zIndex: 20,
+            },
+          ]}
+        />
       </View>
     </SafeAreaView>
   );
@@ -79,23 +120,24 @@ const styles = StyleSheet.create({
   },
   safeaArea_dark: {
     flex: 1,
-    backgroundColor: Colors.primary.s600,
     alignItems: "center",
   },
-  container: {
+  topContainer: {
     width: "90%",
-  },
-  searchToolContainer: {
-    alignItems: "flex-end",
-    marginVertical: Sizing.x10,
   },
   main: {
     flex: 1,
-    width: "95%",
+    width: "100%",
+  },
+  overlay: {
+    position: "absolute",
+    top: 0,
+    width: "100%",
+    height: "100%",
+    backgroundColor: applyOpacity(Colors.neutral.s500, 0.5),
   },
   noEventsMessage: {
     flex: 1,
-    width: "100%",
     alignItems: "center",
     justifyContent: "center",
   },
