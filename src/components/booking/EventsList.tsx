5d4c337e3342d1a3ea5e88168e2822612d7f5b56
Piotr Napierala <piotr.napierala94@gmail.com>
2021-09-27 00:08:06 +0200

    [components/booking] improve performance & add searchEvents

--- src/components/booking/EventsList.tsx
+++ src/components/booking/EventsList.tsx
@@ -13,36 +13,34 @@ import { SubHeaderText } from "components/rnWrappers/subHeaderText";
 import { Colors, Sizing } from "styles/index";
 import { appContext } from "contexts/contextApi";
 
-export interface EventsListProps {}
+export interface EventsListProps {
+  customEvents: any[] | null;
+}
 
-export const EventsList = ({}: EventsListProps) => {
+export const EventsList = ({ customEvents }: EventsListProps) => {
   const { events, isLoading, getEventsPaginated, eventsPage } =
     useEventsPagination();
   const { colorScheme } = appContext();
   const isLightMode = colorScheme !== "dark";
 
-  const renderEventCard = React.useCallback(
-    ({ item }: any) => {
-      const { title, description, selectedDays, imageURI, eventCardColor } =
-        item;
-      const selectedDaysArr: number[] = Object.values(selectedDays ?? {});
-      const fromDate = Math.min(...selectedDaysArr);
-      const toDate = Math.max(...selectedDaysArr);
+  const renderEventCard = React.useCallback(({ item }: any) => {
+    const { title, description, selectedDays, imageURI, eventCardColor } = item;
+    const selectedDaysArr: number[] = Object.values(selectedDays ?? {});
+    const fromDate = Math.min(...selectedDaysArr);
+    const toDate = Math.max(...selectedDaysArr);
 
-      return (
-        <EventsListCard
-          title={title}
-          description={description}
-          fromDate={fromDate}
-          toDate={toDate}
-          image={imageURI}
-          color={eventCardColor}
-          isTransparent={eventCardColor === "transparent"}
-        />
-      );
-    },
-    [events]
-  );
+    return (
+      <EventsListCard
+        title={title}
+        description={description}
+        fromDate={fromDate}
+        toDate={toDate}
+        image={imageURI}
+        color={eventCardColor}
+        isTransparent={eventCardColor === "transparent"}
+      />
+    );
+  }, []);
 
   const keyExtractor = (item: any, index: number) => getRandomKey(index);
   const getItem = (data: any, index: number) => data[index];
@@ -59,24 +57,38 @@ export const EventsList = ({}: EventsListProps) => {
       style={{ paddingTop: Sizing.x35 }}
     />
   );
+  const onEndReach = () => loadEvents(eventsPage + 1, false);
+  const onRefresh = React.useCallback(() => loadEvents(1, true), []);
+  const onLayout = React.useCallback(
+    ({ data, index }) => ({
+      length: Sizing.x130,
+      offset: Sizing.x130 * index,
+      index,
+    }),
+    []
+  );
 
   return (
     <>
       {!!events.length && !isLoading ? (
         <VirtualizedList
-          style={{ flex: 1 }}
-          data={events}
+          style={{ flex: 1, width: "100%" }}
+          contentContainerStyle={{ width: "95%", alignSelf: "center" }}
+          data={customEvents ?? events}
           getItem={getItem}
           refreshing={isLoading}
           initialNumToRender={4}
           onEndReachedThreshold={0.5}
-          onEndReached={() => loadEvents(eventsPage + 1, false)}
+          onEndReached={onEndReach}
           getItemCount={getItemCount}
           renderItem={renderEventCard}
           keyExtractor={keyExtractor}
-          onRefresh={() => loadEvents(1, true)}
+          onRefresh={onRefresh}
+          onLayout={onLayout}
           showsVerticalScrollIndicator={false}
-          ListFooterComponent={_ActivityIndicator}
+          ListFooterComponent={!customEvents ? _ActivityIndicator : null}
+          maxToRenderPerBatch={10}
+          updateCellsBatchingPeriod={30}
           removeClippedSubviews
         />
       ) : !events.length && isLoading ? (
