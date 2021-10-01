26b66a00739fce6b877e125002177d359eaf90bb
Piotr Napierala <piotr.napierala94@gmail.com>
2021-10-02 00:31:40 +0200

    [screens//AvailableDaysSelection] update logic for Google consent/oauth

--- src/screens/organizer/newEvent/AvailableDaysSelection.tsx
+++ src/screens/organizer/newEvent/AvailableDaysSelection.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { View, StyleSheet, Pressable, ScrollView } from "react-native";
+import { View, StyleSheet, Pressable, ScrollView, Linking } from "react-native";
 
 import { CheckIcon, LeftArrowIcon } from "assets/icons";
 import { appContext, eventCreationContext } from "contexts/contextApi";
@@ -13,6 +13,8 @@ import { CalendarWrapperSimple } from "components/calendar/CalendarWrapperSimple
 import { FullWidthButton } from "components/buttons/fullWidthButton";
 import { BodyText } from "components/rnWrappers/bodyText";
 import { useGoogleAuth } from "lib/hooks/useGoogleAuth";
+import { ErrorModal } from "components/modals/errorModal";
+import * as qs from "qs";
 
 type Props = StackScreenProps<
   EventCreationParamList,
@@ -21,109 +23,152 @@ type Props = StackScreenProps<
 
 export const AvailableDaysSelection = ({ navigation }: Props) => {
   const { colorScheme } = appContext();
-  const { selectedDays } = eventCreationContext();
-  const { isRequesting, isFailed, requestAccess } = useGoogleAuth();
+  const { selectedDays, removeSelectedDays, removeSelectedWeeks } =
+    eventCreationContext();
+  const { isRequesting, requestAccess } = useGoogleAuth();
   const [acceptedCheckbox, setAcceptedChecbox] = React.useState<boolean>(false);
+  const [error, setError] = React.useState<any>({ isVisible: false, type: "" });
+
+  const listener = {
+    start: function () {
+      Linking.addEventListener("url", (event) => {
+        console.log(event);
+        const query = qs.parse(event.url.split("?")[1]);
+        const { success } = query;
+
+        if (success === "false") {
+          setError({ isVisible: true, type: "GoogleOauth" });
+        }
+
+        if (success === "true") navigation.navigate("Available Time Selection");
+      });
+    },
+    remove: function () {
+      Linking.removeEventListener("url", this.start);
+    },
+  };
+
+  React.useEffect(() => {
+    listener.start();
+
+    return () => listener.remove();
+  }, []);
 
   const isLightMode = colorScheme === "light";
   const isDisabledBtn =
     selectedDays === null || !Object.entries(selectedDays).length;
-  const onBackNavigationPress = () => navigation.goBack();
-  const onNextButtonPress = React.useCallback(async () => {
-    try {
-      if (acceptedCheckbox) await requestAccess();
-      // navigation.navigate("Available Time Selection");
-    } catch (e) {
-      console.error(e);
-    }
-  }, [acceptedCheckbox]);
   const onCheckBoxPress = () => {
+    setError({ isVisible: false, type: "" });
     setAcceptedChecbox((prev) => !prev);
+  };
+  const onBackNavigationPress = React.useCallback(() => {
+    removeSelectedDays();
+    removeSelectedWeeks();
+    setAcceptedChecbox(false);
+    navigation.goBack();
+  }, []);
+  const onNextButtonPress = async () => {
+    if (error.isVisible) setError({ isVisible: false, type: "" });
+    if (acceptedCheckbox)
+      try {
+        await requestAccess();
+      } catch (e) {}
+    if (!acceptedCheckbox) navigation.navigate("Available Time Selection");
   };
 
   return (
-    <SafeAreaView
-      style={[
-        styles.safeArea,
-        {
-          backgroundColor: isLightMode
-            ? Colors.primary.neutral
-            : Colors.primary.s600,
-        },
-      ]}>
-      <View style={{ flex: 1, width: "100%", alignItems: "center" }}>
-        <View style={styles.navigation}>
-          <Pressable onPress={onBackNavigationPress} hitSlop={10}>
-            <LeftArrowIcon
-              width={24}
-              height={24}
-              color={isLightMode ? Colors.primary.s600 : Colors.primary.neutral}
-            />
-          </Pressable>
-        </View>
-        <View style={{ width: "90%" }}>
-          <HeaderText
-            customStyles={{ marginBottom: Sizing.x10 }}
-            colorScheme={colorScheme}>
-            Select dates you are available to host event
-          </HeaderText>
-        </View>
-        <ScrollView
-          style={{ width: "100%" }}
-          showsVerticalScrollIndicator={false}
-          contentContainerStyle={{ width: "100%", alignItems: "center" }}>
-          <CalendarWrapperSimple>
-            <MonthlyWrapper isNewEventCalendar />
-          </CalendarWrapperSimple>
-          <View style={styles.checkboxWrapper}>
-            <Pressable
-              onPress={onCheckBoxPress}
-              hitSlop={5}
-              style={[
-                styles.checkbox,
-                {
-                  borderWidth: isLightMode ? Outlines.borderWidth.thin : 0,
-                  backgroundColor:
-                    isLightMode && acceptedCheckbox
-                      ? Colors.primary.s600
-                      : Colors.primary.neutral,
-                },
-              ]}>
-              <CheckIcon
-                width="15"
-                height="15"
-                strokeWidth="3.5"
-                stroke={
-                  isLightMode
-                    ? Colors.primary.neutral
-                    : !isLightMode && acceptedCheckbox
-                    ? Colors.primary.s600
-                    : Colors.primary.neutral
+    <>
+      <SafeAreaView
+        style={[
+          styles.safeArea,
+          {
+            backgroundColor: isLightMode
+              ? Colors.primary.neutral
+              : Colors.primary.s600,
+          },
+        ]}>
+        <View style={{ flex: 1, width: "100%", alignItems: "center" }}>
+          <View style={styles.navigation}>
+            <Pressable onPress={onBackNavigationPress} hitSlop={10}>
+              <LeftArrowIcon
+                width={24}
+                height={24}
+                color={
+                  isLightMode ? Colors.primary.s600 : Colors.primary.neutral
                 }
               />
             </Pressable>
-            <BodyText
-              customStyle={{
-                fontFamily: "Roboto-Regular",
-                fontSize: Sizing.x14,
-              }}
-              colors={[Colors.primary.s800, Colors.primary.neutral]}>
-              I would like to sync with my Google's Calendar schedule to
-              automatically prevent unavailable days from being booked (requires
-              Google's account permission).
-            </BodyText>
           </View>
-        </ScrollView>
-        <FullWidthButton
-          text="Next"
-          colorScheme={colorScheme}
-          disabled={isDisabledBtn}
-          onPressCallback={onNextButtonPress}
-          loadingIndicator={isRequesting}
-          style={styles.button}
-        />
-      </View>
-    </SafeAreaView>
+          <View style={{ width: "90%" }}>
+            <HeaderText
+              customStyles={{ marginBottom: Sizing.x10 }}
+              colorScheme={colorScheme}>
+              Select dates you are available to host event
+            </HeaderText>
+          </View>
+          <ScrollView
+            style={{ width: "100%" }}
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{ width: "100%", alignItems: "center" }}>
+            <CalendarWrapperSimple>
+              <MonthlyWrapper isNewEventCalendar />
+            </CalendarWrapperSimple>
+            <View style={styles.checkboxWrapper}>
+              <Pressable
+                onPress={onCheckBoxPress}
+                hitSlop={5}
+                style={[
+                  styles.checkbox,
+                  {
+                    borderWidth: isLightMode ? Outlines.borderWidth.thin : 0,
+                    backgroundColor:
+                      isLightMode && acceptedCheckbox
+                        ? Colors.primary.s600
+                        : Colors.primary.neutral,
+                    borderColor:
+                      isLightMode && acceptedCheckbox
+                        ? Colors.primary.s600
+                        : "black",
+                  },
+                ]}>
+                <CheckIcon
+                  width="15"
+                  height="15"
+                  strokeWidth="3.5"
+                  stroke={
+                    isLightMode
+                      ? Colors.primary.neutral
+                      : !isLightMode && acceptedCheckbox
+                      ? Colors.primary.s600
+                      : Colors.primary.neutral
+                  }
+                />
+              </Pressable>
+              <BodyText
+                customStyle={{
+                  fontFamily: "Roboto-Regular",
+                  fontSize: Sizing.x14,
+                  width: "90%",
+                }}
+                colors={[Colors.primary.s800, Colors.primary.neutral]}>
+                Attendees schedule time on my Google calendar
+                {"\n"}
+                Next: grant access
+              </BodyText>
+            </View>
+          </ScrollView>
+          <FullWidthButton
+            text="Next"
+            colorScheme={colorScheme}
+            disabled={isDisabledBtn}
+            onPressCallback={onNextButtonPress}
+            loadingIndicator={isRequesting}
+            style={styles.button}
+          />
+        </View>
+      </SafeAreaView>
+      <ErrorModal isModalVisible={error.isVisible} errorType={error.type} />
+    </>
   );
 };
 
