83f42a9b2c5ff29b4781eddb181c20e3f97e6496
Piotr Napierala <piotr.napierala94@gmail.com>
2021-10-28 01:11:11 +0200

    [MyCalendar] add logic for fetching events with `onMonthChange`

--- src/containers/MyCalendar.tsx
+++ src/containers/MyCalendar.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
+import { CalendarEventsList, MonthlyWrapper } from "components/calendar";
 import {
-  CalendarEventsList,
-  CalendarWrapper,
-  MonthlyWrapper,
-} from "components/calendar";
-import { appContext } from "contexts/contextApi";
+  appContext,
+  eventCreationContext,
+  myCalendarContext,
+} from "contexts/contextApi";
 import {
   ActivityIndicator,
   Pressable,
@@ -17,6 +17,7 @@ import { useNavigation } from "@react-navigation/native";
 import { PlusIcon } from "assets/icons";
 import { ProfileContext } from "contexts/profileContext";
 import { useCalendarEvents } from "lib/hooks/useCalendarEvents";
+import { monthsByName } from "common/types/calendarTypes";
 
 export interface CalendarProps {
   isBookingCalendar?: boolean;
@@ -30,19 +31,73 @@ export const Calendar = ({
   isRegularCalendar,
 }: CalendarProps) => {
   const { colorScheme } = appContext();
+  const { resetState } = eventCreationContext();
   const { id } = React.useContext(ProfileContext);
-  const { isLoading, events } = useCalendarEvents(id);
+  const { events, calendarHeader } = myCalendarContext();
+  const { getEvents } = useCalendarEvents(id);
+  const [isLoading, setIsLoading] = React.useState(true);
+
   const isLightMode = colorScheme === "light";
   const navigation = useNavigation();
 
+  React.useEffect(() => {
+    if (events === null)
+      (async () => {
+        await getEvents();
+      })();
+
+    setIsLoading(false);
+
+    const subscribe = () => {
+      navigation.addListener("blur", () => {
+        console.log("blur");
+      });
+      navigation.addListener("focus", async () => {
+        console.log("focus");
+
+        setIsLoading(true);
+        await getEvents(
+          new Date(calendarHeader.year, monthsByName[calendarHeader.month])
+        );
+        setIsLoading(false);
+      });
+    };
+
+    const unsubscribe = () => {
+      navigation.removeListener("blur", () => {});
+      navigation.removeListener("focus", () => {});
+    };
+
+    subscribe();
+
+    return unsubscribe;
+  }, []);
+
   const onAddEventPress = () => {
+    resetState();
     navigation.navigate("New Event Description");
   };
 
+  const onMonthChange = async () => {
+    if (events) setIsLoading(true);
+
+    try {
+      await getEvents(
+        new Date(calendarHeader.year, monthsByName[calendarHeader.month])
+      );
+
+      if (!events) return;
+
+      setIsLoading(false);
+    } catch (e) {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <CalendarWrapper>
+    <>
       <MonthlyWrapper
-        isBookingCalendar={isBookingCalendar}
+        customCallback={onMonthChange}
         isRegularCalendar={isRegularCalendar}
       />
       {(isBookingCalendar == null || isHomeScreen) &&
@@ -93,7 +148,7 @@ export const Calendar = ({
             </Pressable>
           </View>
         ))}
-    </CalendarWrapper>
+    </>
   );
 };
 
