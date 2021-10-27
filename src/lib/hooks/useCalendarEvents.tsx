17c7a64d17170361d8451db7edc8950048f932a1
Piotr Napierala <piotr.napierala94@gmail.com>
2021-10-28 01:09:22 +0200

    [hooks] add event fetching to useCalendarEvents

--- src/lib/hooks/useCalendarEvents.tsx
+++ src/lib/hooks/useCalendarEvents.tsx
@@ -1,24 +1,34 @@
-import { Events } from "Api/Events";
+import { Users } from "Api/Users";
 import { myCalendarContext } from "contexts/contextApi";
+import { convertToCalendarEvents } from "lib/utils";
 import * as React from "react";
 
 export const useCalendarEvents = (id: string) => {
-  const [isLoading, setIsLoading] = React.useState<boolean>(false);
-  const { events, setEvents } = myCalendarContext();
+  const { setEvents } = myCalendarContext();
+
+  const getEvents = async (currCalendarDate?: Date) => {
+    setEvents(null);
+
+    try {
+      let res = await Users.getUserCalendarEvents(id, currCalendarDate);
+
+      if (res) {
+        res = convertToCalendarEvents(res);
+
+        if (res && res.length) setEvents(res);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   React.useEffect(() => {
-    async () => {
-      try {
-        setIsLoading(true);
-        const res = await Events.getEventsByUserId();
-      } catch (e) {
-        console.error(e);
-      }
-    };
+    // if (!events) {
+    // setIsLoading(true);
+    // }
   }, []);
 
   return {
-    isLoading,
-    events,
+    getEvents,
   };
 };
